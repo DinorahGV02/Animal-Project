e365db0f9acf1a3ecf11929855b0d9c0e62c7d95
Camilo Romero <91925901+RomeroCamilo@users.noreply.github.com>
2022-04-11 13:58:12 -0400

    Update animalphp.php

--- animalphp.php
+++ animalphp.php
@@ -1,6 +1,6 @@
 <? php
 $connect = mysqli_connect("localhost","root","root");
-if(!$connect) die("Oopsie ERROR");
+if(!$connect) die("ERROR");
 $sql =
 		"INSERT INTO Animals.animal (animal_name, habitat, animal_type)
 		VALUES ('".$_REQUEST['animalName']."','".$_REQUEST['animalType']."','".$_REQUEST['habitat']."')";
