7e539b4ac49a1f7f3a3fecd62746f712225e33e5
Dinorah <83092931+DinorahGV02@users.noreply.github.com>
2022-04-11 13:26:07 -0400

    Add files via upload

---
+++ animalphp.php
@@ -0,0 +1,13 @@
+<? php
+$connect = mysqli_connect("localhost","root","root");
+if(!$connect) die("Oopsie ERROR");
+$sql =
+		"INSERT INTO Animals.animal (animal_name, habitat, animal_type)
+		VALUES ('".$_REQUEST['animalName']."','".$_REQUEST['animalType']."','".$_REQUEST['habitat']."')";
+		$result = mysqli_query($connect, $sql);
+		if($result)
+			echo "ANIMAL INSERTED";
+		else
+			echo "ANIMAL NOT INSERTED";
+
+<?
---
+++ animalsql.sql
@@ -0,0 +1,7 @@
+create database Animals;
+use Animals;
+CREATE TABLE animal(
+animal_name varchar(30) primary key,
+habitat varchar(30),
+animal_type varchar(30)
+);
